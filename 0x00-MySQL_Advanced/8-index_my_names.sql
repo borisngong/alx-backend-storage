edbcf523b55ab8fbf528bf486b27a03b7d9c8a0f
borisngong <borisngong@gmail>
2024-10-17 12:40:30 -0700

    A SQL script that creates an index

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,7 +1,2 @@
 -- Script that creates an idx on the 1st letter of name column
-
--- Drop the index if it already exists (to avoid conflicts)
-DROP INDEX IF EXISTS idx_name_first ON names;
-
--- Create the index on the first letter of the name column
 CREATE INDEX idx_name_first ON names(name(1));
